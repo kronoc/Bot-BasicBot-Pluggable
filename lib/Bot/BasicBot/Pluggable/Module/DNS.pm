29235a29d088691c5ec4a085679298e40649c78e
Mario Domgörgen <mdom@cpan.org>
2009-08-15 15:39:39 +0000

    Import of MDOM/Bot-BasicBot-Pluggable-0.80 from CPAN.
    gitpan-cpan-distribution: Bot-BasicBot-Pluggable
    gitpan-cpan-version:      0.80
    gitpan-cpan-path:         MDOM/Bot-BasicBot-Pluggable-0.80.tar.gz
    gitpan-cpan-author:       MDOM
    gitpan-cpan-maturity:     released

--- lib/Bot/BasicBot/Pluggable/Module/DNS.pm
+++ lib/Bot/BasicBot/Pluggable/Module/DNS.pm
@@ -18,7 +18,7 @@ Returns the IP address of the hostname.
 
 =head1 AUTHOR
 
-Tom Insam <tom@jerakeen.org>
+Mario Domgoergen <mdom@cpan.org>
 
 This program is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
--- lib/Bot/BasicBot/Pluggable/Module/Title.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Title.pm
@@ -28,7 +28,7 @@ L<URI::Find::Simple>
 
 =head1 AUTHOR
 
-Tom Insam <tom@jerakeen.org>
+Mario Domgoergen <mdom@cpan.org>
 
 This program is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
